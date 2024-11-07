1ac82ff4809be3ebbefa12fa1e92138767a1029e
Beforerr <zzj956959688@gmail.com>
2024-11-07 13:16:33 -0800

    update: wind configuration to use  magnetic variable

--- src/discontinuitypy/mission/wind.py
+++ src/discontinuitypy/mission/wind.py
@@ -5,9 +5,10 @@ __all__ = ['WindConfigBase']
 
 # %% ../../../notebooks/missions/wind.ipynb 0
 from ..datasets import IDsDataset
-from space_analysis.meta import MagDataset, TempDataset, PlasmaDataset
+from space_analysis.core import MagVariable
+from space_analysis.meta import TempDataset, PlasmaDataset
 from space_analysis.missions.wind import (
-    wi_mfi_h2_ds,
+    wi_mfi_h2_bgse,
     wi_pm_3dp_ds,
     wi_plsp_3dp_ds,
     wi_elm2_3dp_ds,
@@ -16,7 +17,7 @@ from space_analysis.missions.wind import (
 # %% ../../../notebooks/missions/wind.ipynb 1
 class WindConfigBase(IDsDataset):
     name: str = "Wind"
-    mag_meta: MagDataset = wi_mfi_h2_ds
+    mag_meta: MagVariable = wi_mfi_h2_bgse
     plasma_meta: PlasmaDataset = wi_pm_3dp_ds
     ion_temp_meta: TempDataset = wi_plsp_3dp_ds
     e_temp_meta: TempDataset = wi_elm2_3dp_ds
